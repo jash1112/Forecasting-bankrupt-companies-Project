110efc82cedea81f22f2ec985e24c5988528a12c
John Anh Nguyen <John.nguyen.TA@gmail.com>
2024-04-19 18:39:07 -0400

    upload 2 prototype of web page

--- app/app.py
+++ app/app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template("index.html")
+   return render_template("index(prototype_1).html")
 
+@app.route('/second')
+def second():
+   return render_template("index_prototype_2.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 
